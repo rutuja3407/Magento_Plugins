1cc67445ba17feac9c6073503d97427224bd135e
rutuja3407 <64635221+rutuja3407@users.noreply.github.com>
2024-03-27 16:01:00 +0530

    Delete SAML Premium directory

--- SAML Premium/SP/Logger/Logger.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-namespace MiniOrange\SP\Logger;
-
-class Logger extends \Monolog\Logger
-{
-}
--- SAML Premium/SP/registration.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-
-\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, "\115\151\156\151\117\162\x61\x6e\147\145\137\x53\x50", __DIR__);
